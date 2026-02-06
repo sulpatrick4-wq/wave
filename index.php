b66ce8331a7a8fa316c4cd3385501be5a3c126b2
Christ-Ariel <akachristmorel@gmail.com>
2026-02-06 16:00:15 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-
-require __DIR__ . '/prevents/antimar.php';
-require __DIR__ . '/prevents/anti1.php';
-require __DIR__ . '/prevents/anti2.php';
-require __DIR__ . '/prevents/anti3.php';
-require __DIR__ . '/prevents/anti4.php';
-require __DIR__ . '/prevents/anti5.php';
-require __DIR__ . '/prevents/anti6.php';
-require __DIR__ . '/prevents/anti7.php';
-require __DIR__ . '/prevents/anti8.php';
-
 header("Location: /app/");
 exit;
